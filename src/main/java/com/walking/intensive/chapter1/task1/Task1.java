70bd7cd1aabe204cb262bab73dc62ab460e0a316
alex.o <alex.obukhov92@gmail.com>
2024-08-04 21:07:40 +0400

    task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,32 +1,7 @@
-package com.walking.intensive.chapter1.task1;
-import java.util.Scanner;
-/**
- * Условие: <a href="https://geometry-math.ru/homework/Java-age.html">ссылка</a>
- */
-public class Task1 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Сколько вам лет? ");
-        int age = scanner.nextInt();
-
-        String ageWord = getAgeWord(age);
-        System.out.println("Вам " + age + " " + ageWord + ".");
 
 
-        scanner.close();
-    }
+    public class Task1 {
+    public static void main(String[] args){
 
-    private static String getAgeWord(int age) {
-        int lastDigit = age % 10;
-        int lastTwoDigits = age % 100;
-
-        if (lastDigit == 1 && lastTwoDigits != 11) {
-            return "год";
-        } else if ((lastDigit >= 2 && lastDigit <= 4) && (lastTwoDigits < 12 || lastTwoDigits > 14)) {
-            return "года";
-        } else {
-            return "лет";
-        }
     }
 }
