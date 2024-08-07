0030a69654693f534bdfb31202e0a098fe9c3d3b
alex.o <alex.obukhov92@gmail.com>
2024-08-07 20:38:56 +0400

    task2.1 сделал подсчет подьезда

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -3,61 +3,68 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {// класс task2
 
     public static void main(String[] args) { // Метод main
-        int floorAmount = 2;
-        int entranceAmount = 2;
-        int flatNumber = 11;
+        int floorAmount = 4;
+        int entranceAmount = 6;
+        int flatNumber = 20;
         String result = getFlatLocation(floorAmount, entranceAmount, flatNumber);
         System.out.println(result);
 
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) { // Метод get...
-        if (flatNumber > 16 || flatNumber <= 0) {
-            String result = " Такой квартиры нет.";
-            return result;
+        int floorCapacity = 4;
+        int entrenceCapacity = floorAmount * floorCapacity;
+        int flatQuantity = floorAmount * entranceAmount * floorCapacity;
+        if (flatNumber > flatQuantity || flatNumber <= 0) {
+            return "Такой квартиры нет.";
         }
-        if (flatNumber <= 8) {
-            entranceAmount = 1;
+        int i = 0;  // надо решить через условия
+        while (true) {
+            i++;
+            if (flatNumber <= entrenceCapacity * i) {
+                break;
+            }
+            //entrenceCapacity += entrenceCapacity;
+        }
 
-        }
         if (flatNumber <= 4) {
-            floorAmount = 1;
+            floorAmount = 1; //
         } else if (flatNumber >= 9 && flatNumber <= 12) {
             floorAmount = 1;
 
         }
-        String pos;
+        String flatPosition;
         switch (flatNumber) {
             case 1:
             case 5:
             case 9:
             case 13:
-                pos = "слева от лифта, влево";
+                flatPosition = "слева от лифта, влево";
                 break;
             case 2:
             case 6:
             case 10:
             case 14:
-                pos = "слева от лифта, вправо";
+                flatPosition = "слева от лифта, вправо";
                 break;
             case 3:
             case 7:
             case 11:
             case 15:
-                pos = "справа от лифта, влево";
+                flatPosition = "справа от лифта, влево";
                 break;
             case 4:
             case 8:
             case 12:
             case 16:
-                pos = "справа от лифта, вправо";
+                flatPosition = "справа от лифта, вправо";
                 break;
             default:
-                pos = "";
+                flatPosition = "";
         }
 
 
-        String result = flatNumber + " кв - " + entranceAmount + " подъезд " + floorAmount + " этаж " + pos;
+        String result = flatNumber + " кв - " + i + " подъезд " + floorAmount + " этаж " + flatPosition;
 
         return result; // Заглушка. При реализации - удалить
     }
