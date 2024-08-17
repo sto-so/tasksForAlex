ad8f4aaca92703419b2e1d6ccda8db68beaf1e3e
alex.o <alex.obukhov92@gmail.com>
2024-08-17 21:35:18 +0400

    task2.2 окончательное решение без циклов.

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -1,53 +1,45 @@
 package com.walking.intensive.chapter1.task2;
 
 public class Task2 {
-    public static void main(String[] args) { // Метод main
+    public static void main(String[] args) {
         int floorAmount = 4;
         int entranceAmount = 4;
-        int flatNumber = 64;
-        String result = getFlatLocation(floorAmount, entranceAmount, flatNumber);
-        System.out.println(result);
+        int flatNumber = 30;
 
+        try {
+            System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
-    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) { // Метод get...
+    static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         int floorCapacity = 4;
         int entrenceCapacity = floorAmount * floorCapacity;
         int flatQuantity = floorAmount * entranceAmount * floorCapacity;
+
         if (flatNumber > flatQuantity || flatNumber <= 0) {
             return "Такой квартиры нет.";
         }
-        int entranceNumber = (flatNumber / entrenceCapacity);
-        int currentFloor = (flatNumber - (entranceNumber * entrenceCapacity)) / floorCapacity;
 
-        if (flatNumber == flatQuantity) {
-            --entranceNumber;
-            currentFloor = --entranceAmount;
+        int currentEntrance = flatNumber / entrenceCapacity;
+        int currentFloor = flatNumber / floorCapacity % floorAmount;
 
+        if (flatNumber != flatQuantity) {
+            currentEntrance = currentEntrance + 1;
+            currentFloor = currentFloor + 1;
         }
 
-        int flatPos = flatNumber % floorCapacity;
-        String flatPosition = null;
-        switch (flatPos) {
-            case 1:
-                flatPosition = "слева от лифта, влево";
-                break;
-            case 2:
-                flatPosition = "слева от лифта, вправо";
-                break;
-            case 3:
-                flatPosition = "справа от лифта, влево";
-                break;
-            case 4:
-            case 0:
-                flatPosition = "справа от лифта, вправо";
-                break;
-            default:
-                flatPosition = "";
-        }
-        String result = flatNumber + " кв - " + (entranceNumber + 1) + " подъезд " + (currentFloor + 1) + " этаж " + flatPosition;
+        int flatRemains = flatNumber % floorCapacity;
+        String flatPosition = switch (flatRemains) {
+            case 1 -> "слева от лифта, влево";
+            case 2 -> "слева от лифта, вправо";
+            case 3 -> "справа от лифта, влево";
+            case 0 -> "справа от лифта, вправо";
+            default -> throw new IllegalArgumentException("Лезь искать в switch case.");
+        };
 
-        return result; // Заглушка. При реализации - удалить
+        return flatNumber + " кв - " + currentEntrance + " подъезд " + currentFloor + " этаж " + flatPosition;
     }
 }
 
